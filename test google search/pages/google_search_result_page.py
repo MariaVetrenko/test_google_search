890cf541e53facc2150311b650cf19daead31b3c
MariaVetrenko <124521255+MariaVetrenko@users.noreply.github.com>
2023-02-06 23:48:47 +0400

    Delete test google search directory

--- test google search/pages/google_search_result_page.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import unittest
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from pages.common_page import CommonPage
-
-
-class GoogleSearchResultPage(CommonPage):
-    first_found_title = "//*[@class='LC20lb MBeuO DKV0Md'][1]"
--- test google search/test_data.py
+++
@@ -1,2 +0,0 @@
-LINK = "https://www.google.ru/"
-TEST_SEARCH_TEXT = "Automation QA"
